900817b823f7a46ea9df12e95c5620fd9fe48ace
honksJr <68715413+honksJr@users.noreply.github.com>
2020-07-25 14:46:18 -0700

    Create dogyears.js

---
+++ dog-years/dogyears.js
@@ -0,0 +1,8 @@
+let myAge = 18;
+let earlyYears = 2;
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+const myName = 'HonkS'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
